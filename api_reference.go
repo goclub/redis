97e0752d631e725c06d23fca0a219dc188ee8352
Nimo <nimo.jser@gmail.com>
2021-02-15 20:12:06 +0800

    Create api_reference.go

---
+++ api_reference.go
@@ -0,0 +1,36 @@
+package red
+
+type APIReference struct {
+	Strings struct{
+		GET GET
+		SET SET
+		SETNX SETNX
+		SETXX SETXX
+		DEL DEL
+		DECR DECR
+		INCR INCR
+	}
+	Lists struct{
+		LPUSH LPUSH
+		LPUSHX LPUSHX
+		RPUSH RPUSH
+		RPUSHX RPUSHX
+		LPOP LPOP
+		LPOPCount LPOPCount
+		RPOP RPOP
+		RPOPCount RPOPCount
+		LRANGE LRANGE
+		BRPOPLPUSH BRPOPLPUSH
+	}
+	Hashes struct{
+		HGET HGET
+		HMGET HMGET
+		HSET HSET
+	}
+	Stream struct{
+		XADD XADD
+		XLEN XLEN
+		XRANGE XRANGE
+		XDEL XDEL
+	}
+}
