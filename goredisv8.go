3bd4b7a56ef74cc9327c15d19b9f3547c27735f5
nimoc <nimocc@126.com>
2021-08-08 12:52:22 +0800

    without nil

--- goredisv8.go
+++ goredisv8.go
@@ -2,8 +2,8 @@ package red
 
 import (
 	"context"
-	"errors"
 	"github.com/go-redis/redis/v8"
+	xerr "github.com/goclub/error"
 )
 
 func ArgsToInterfaces(args []string) (interfaces []interface{}) {
@@ -21,9 +21,12 @@ func NewGoRedisV8(goredisV8Client redis.UniversalClient) GoRedisV8 {
 	}
 }
 func (r GoRedisV8) DoStringReply(ctx context.Context, args []string) (reply string, isNil bool, err error) {
+	defer func() {
+		if err != nil { err = xerr.WithStack(err) }
+	}()
 	cmd := r.Core.Do(ctx, ArgsToInterfaces(args)...)
 	err = cmd.Err() ; if err != nil {
-		if errors.Is(err, redis.Nil) {
+		if xerr.Is(err, redis.Nil) {
 			return "", true, nil
 		}
 		return
@@ -31,11 +34,25 @@ func (r GoRedisV8) DoStringReply(ctx context.Context, args []string) (reply stri
 	reply = cmd.Val().(string)
 	return
 }
+func (r GoRedisV8) DoStringReplyWithoutNil(ctx context.Context, args []string) (reply string, err error) {
+	var isNil bool
+	reply, isNil, err = r.DoStringReply(ctx, args) ; if err != nil {
+	    return
+	}
+	if isNil == true {
+		err = xerr.New("DoStringReplyWithoutNil(ctx, args) args exec result can not be nil")
+		return
+	}
+	return
+}
 
 func (r GoRedisV8) DoIntegerReply(ctx context.Context, args []string) (reply int64, isNil bool, err error) {
+	defer func() {
+		if err != nil { err = xerr.WithStack(err) }
+	}()
 	cmd := r.Core.Do(ctx, ArgsToInterfaces(args)...)
 	err = cmd.Err() ; if err != nil {
-		if errors.Is(err, redis.Nil) {
+		if xerr.Is(err, redis.Nil) {
 			return 0, true, nil
 		}
 		return
@@ -43,8 +60,21 @@ func (r GoRedisV8) DoIntegerReply(ctx context.Context, args []string) (reply int
 	reply = cmd.Val().(int64)
 	return
 }
-
+func (r GoRedisV8) DoIntegerReplyWithoutNil(ctx context.Context, args []string) (reply int64, err error) {
+	var isNil bool
+	reply, isNil, err = r.DoIntegerReply(ctx, args) ; if err != nil {
+		return
+	}
+	if isNil == true {
+		err = xerr.New("DoIntegerReply(ctx, args) args exec result can not be nil")
+		return
+	}
+	return
+}
 func (r GoRedisV8) DoArrayIntegerReply(ctx context.Context, args []string)(reply ArrayInteger, err error) {
+	defer func() {
+		if err != nil { err = xerr.WithStack(err) }
+	}()
 	cmd := r.Core.Do(ctx, ArgsToInterfaces(args)...)
 	err = cmd.Err() ; if err != nil {
 		return
@@ -54,6 +84,9 @@ func (r GoRedisV8) DoArrayIntegerReply(ctx context.Context, args []string)(reply
 }
 
 func (r GoRedisV8) DoArrayStringReply(ctx context.Context, args []string)(reply ArrayString, err error) {
+	defer func() {
+		if err != nil { err = xerr.WithStack(err) }
+	}()
 	cmd := r.Core.Do(ctx, ArgsToInterfaces(args)...)
 	err = cmd.Err() ; if err != nil {
 		return
@@ -63,18 +96,32 @@ func (r GoRedisV8) DoArrayStringReply(ctx context.Context, args []string)(reply 
 }
 
 func (r GoRedisV8) Eval(ctx context.Context, data Script) (reply interface{}, isNil bool, err error) {
+	defer func() {
+		if err != nil { err = xerr.WithStack(err) }
+	}()
 	var argv []interface{}
 	for _, s := range data.Argv {
 		argv = append(argv, s)
 	}
 	cmd := r.Core.Eval(ctx, data.Script, data.Keys, argv...)
 	err = cmd.Err() ; if err != nil {
-		if errors.Is(err, redis.Nil) {
+		if xerr.Is(err, redis.Nil) {
 			return "",true, nil
 		} else {
 			return "",false, err
 		}
 	}
 	reply = cmd.Val()
+	return
+}
+func (r GoRedisV8) EvalWithoutNil(ctx context.Context, data Script) (reply interface{}, err error) {
+	var isNil bool
+	reply, isNil, err = r.Eval(ctx, data) ; if err != nil {
+		return
+	}
+	if isNil == true {
+		err = xerr.New("DoIntegerReply(ctx, args) args exec result can not be nil")
+		return
+	}
 	return
 }
