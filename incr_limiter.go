b76d3a3deaa8ad2a14f6d72abce7216b67673cce
Grifree <1149290648@qq.com>
2022-05-23 11:32:49 +0800

    IncrLimiter

---
+++ incr_limiter.go
@@ -0,0 +1,77 @@
+package red
+
+import (
+	"context"
+	xerr "github.com/goclub/error"
+	"log"
+	"strconv"
+	"time"
+)
+
+// IncrLimiter 递增限制器
+// eg:消息队列:重新入队:消息ID作为key在1分钟内只能递增3次.三次内返回的Limited为false，超过三次为true
+type IncrLimiter struct {
+	Namespace string        `note:"命名空间" eg:"mq_requeue:{messageID}"`
+	Expire    time.Duration `note:"有效期" eg:"1m"`
+	Maximum   uint64        `note:"最大限制" eg:"3"`
+}
+
+func (v IncrLimiter) Do(ctx context.Context, client Connecter) (limited bool, err error) {
+	// 参数校验
+	if v.Namespace == "" {
+		return false, xerr.New("goclub/redis: IncrLimiter{}.Namespace can not be empty string")
+	}
+	if v.Expire.Milliseconds() < 1 {
+		return false, xerr.New("goclub/redis: IncrLimiter{}.Expire can not less 1 millisecond")
+	}
+	if v.Maximum < 1 {
+		return false, xerr.New("goclub/redis: IncrLimiter{}.Maximum can not less 1")
+	}
+	// 递增脚本
+	var reply Reply
+	var isNil bool
+	reply, isNil, err = client.Eval(ctx, Script{
+		KEYS: []string{
+			/*1*/ v.Namespace,
+		},
+		ARGV: []string{
+			/*1*/ strconv.FormatInt(v.Expire.Milliseconds(), 10),
+			/*2*/ strconv.FormatUint(v.Maximum, 10),
+		},
+		Script: `
+			local namespace = KEYS[1]
+			local expire = ARGV[1]
+			local maximun = tonumber(ARGV[2])
+			
+			local num = redis.call("GET", namespace)
+			if num ~= false and tonumber(num) >= maximun then
+				return false
+			end
+
+			local newNum = redis.call("INCR", namespace)
+			if newNum == 1 then
+				redis.call("PEXPIRE", namespace, expire)
+			end
+			return "OK"
+		`,
+	})
+	// 没有成功递增
+	if isNil {
+		limited = true
+		return
+	}
+	// 成功递增
+	replyString, err := reply.String()
+	if err != nil {
+		return
+	}
+	switch replyString {
+	case "OK":
+		limited = false
+		return
+	default:
+		err = xerr.Errorf("goclub/redis: IncrLimiter{}.Do() redis eval reply unexpected", replyString)
+	}
+	log.Printf("reply: %+v , %#v , %+v, %+v, %+v", reply, reply, isNil, reply.Value == "OK", reply.Value == false)
+	return
+}
---
+++ incr_limiter_test.go
@@ -0,0 +1,59 @@
+package red
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestIncrLimiter_Do(t *testing.T) {
+	ctx := context.TODO()
+	for i, client := range Connecters {
+		if i == 0 {
+			_, err := DEL{
+				Keys: []string{"incr_limiter_alarm_1"},
+			}.Do(ctx, client)
+			assert.NoError(t, err)
+		}
+		incrLimiter_Do(t, client)
+	}
+}
+
+func incrLimiter_Do(t *testing.T, client Connecter) {
+	ctx := context.TODO()
+	alarm_1 := IncrLimiter{
+		Namespace: "incr_limiter_alarm_1",
+		Expire:    time.Second * 10,
+		Maximum:   3,
+	}
+	/* 第1次 */
+	limited, err := alarm_1.Do(ctx, client);assert.NoError(t, err)
+	assert.Equal(t, limited, false)
+	// 验证过期时间
+	result, err := PTTL{
+		Key: "incr_limiter_alarm_1",
+	}.Do(ctx, client);assert.NoError(t, err)
+	assert.Less(t, result.TTL.Milliseconds(), alarm_1.Expire.Milliseconds())
+	/* 第2次 */
+	time.Sleep(time.Second)
+	limited, err = alarm_1.Do(ctx, client);assert.NoError(t, err)
+	assert.Equal(t, limited, false)
+	/* 第3次 */
+	time.Sleep(time.Second)
+	limited, err = alarm_1.Do(ctx, client);assert.NoError(t, err)
+	assert.Equal(t, limited, false)
+	/* 第4次 */
+	time.Sleep(time.Second)
+	limited, err = alarm_1.Do(ctx, client);assert.NoError(t, err)
+	assert.Equal(t, limited, true)
+	/* 第5次 */
+	time.Sleep(time.Second)
+	limited, err = alarm_1.Do(ctx, client);assert.NoError(t, err)
+	assert.Equal(t, limited, true)
+	/* 第6次 */
+	time.Sleep(time.Second*7) // 等待过期
+	limited, err = alarm_1.Do(ctx, client);assert.NoError(t, err)
+	assert.Equal(t, limited, false)
+	// 验证ppl 小于20分钟
+}
